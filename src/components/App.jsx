d57f4ed45bdeb5bbe41d71e9fc12cf3e94b1f6cf
Agustin Di Giambatista <agustindigiambatista@gmail.com>
2023-04-18 18:30:02 -0300

    V 0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,10 +4,14 @@ import '../Media-Querys.css'
 function App() {
   return (
     <header className="App">
-      <img className='App-logo' src="./src/assets/logo.svg" alt="logo" />
-      <h1  className='App-title'>Shoppee Market</h1>
+      <img className='App-logo' src="./src/assets/bag.svg" alt="logo" />
+      <h1 onClick={Click} className='App-title'>Shoppee Market</h1>
     </header>
   )
+}
+
+function Click(){
+return alert("Anda Ok")
 }
 
 export default App
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import './Menu.css' 
 
+
+
 export function Menu() {
   return (
     <div>
-       <ul className='Menu-content'>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Tienda</a></li>
-        <li><a href="#">Contacto</a></li>
-       </ul>
+      <button id='abrir' className='abrir-menu'>Abrir</button>
+      <nav className='Menu-nav' id='nav'>
+        <button id='cerrar' className='cerrar-menu'>Cerrar</button>
+        <ul className='Menu-content'>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Tienda</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+      </nav>
     </div>
   )
 }
+
+
 
 export default Menu
